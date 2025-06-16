e7659b9bf7f2959463f52683affc392fc622e661
太田 薫 <hardrock.is.my.life@outlook.com>
2025-06-16 23:05:04 +0900

    Githubへ更新チェックを行うためのStub

---
+++ DontSleepYet/Contracts/Services/IUpdateCheckService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DontSleepYet.Contracts.Services;
+
+public interface IUpdateCheckService
+{
+    Task<UpdateCheckData> CheckUpdateAsync();
+}
+
+public class UpdateCheckData
+{
+    public bool IsUpdateAvailable { get; }
+    public string? LatestVersion { get; }
+    public string? Description { get; }
+    public Uri? InfoUrl { get; }    
+}
---
+++ DontSleepYet/Services/GithubUpdateCheckService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DontSleepYet.Contracts.Services;
+
+namespace DontSleepYet.Services;
+
+public class GithubUpdateCheckService : IUpdateCheckService
+{
+    public Task<UpdateCheckData> CheckUpdateAsync() => throw new NotImplementedException();
+}
